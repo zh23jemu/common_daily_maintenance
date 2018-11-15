5815908bae9edc7cce5d7e091a047f96d39afebb
zh23jemu <41720280+zh23jemu@users.noreply.github.com>
2018-11-15 15:48:37 +0800

    add Get-FolderAcl

---
+++ Get-FolderAcl.ps1
@@ -0,0 +1,6 @@
+﻿# Run on local server
+$folders = (Get-ChildItem E:\GroupsASI\*,E:\GroupsASI\*\*,E:\GroupsASI\*\*\* -Directory).FullName
+foreach ($item in $folders)
+{
+    Get-Acl $item | select PSPath,AccessToString, Owner |Export-Csv d:\groupasi.csv -NoTypeInformation -Append
+}
